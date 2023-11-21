87a3fc50b5dd626b98e79dcd60af23bb480ef544
KEHhyeon <kth050213@naver.com>
2023-11-21 11:37:20 +0900

    Feat: add toggle api

--- back/routes/routes.go
+++ back/routes/routes.go
@@ -3,11 +3,13 @@ package routes
 import (
 	controller "go-api/todolist/contorller"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Run(address string) error {
 	router := gin.Default()
+	router.Use(cors.Default())
 	v1 := router.Group("/")
 	controller.GetTodoRoutes(v1)
 	return router.Run(address)
